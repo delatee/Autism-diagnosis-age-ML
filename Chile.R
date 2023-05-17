5852ca16b6a5d658d9c8243a1976a0ff74116211
Adele Tyson <delatee@gmail.com>
2023-05-17 18:13:23 +0100

    Prevalence by region

--- Chile.R
+++ Chile.R
@@ -237,22 +237,118 @@ summary(log_fit)
 # Josephine Barbaro, Nancy Sadka, Melissa Gilbert, et al
 # https://jamanetwork.com/journals/jamanetworkopen/fullarticle/2789926
 
+chile_bayes <- chile %>%
+  filter(age_june30 >= 6 & age_june30 <= 18,
+  #special_needs_status == 1,
+  sex != 0) %>%
+  select(#school_code,
+    school_region_code,
+    school_rurality_code,
+    teaching_code1,
+    grade_code1,
+    grade_letter, 
+    #student_id,
+    sex,
+    age_june30,
+    #special_needs_status,
+    special_needs_code,
+    student_region_code,
+    #economic_sector_code,
+    #teaching_code_new
+  ) %>%
+  mutate(autism = ifelse(special_needs_code == 105, 1, 0))
+
+# Prevalence of autism in Chile dataset
+sum(chile_bayes$autism) / nrow(chile_bayes) # 0.0047 = 0.47%, very low
+
+# Say autism has mean prevalence of 3% and we are 95% confidence that the prevalence is between 2% and 4%.
+# Then mu = 0.03, sigma = (0.04-0.02) / (2*1.96)
+theta_mu <- 0.03
+theta_sigma <- (0.04-0.02) / (2*1.96)
+theta_a <- theta_mu * (theta_mu * (1-theta_mu) / theta_sigma^2 - 1)
+theta_b <- (1 - theta_mu) * (theta_mu * (1-theta_mu) / theta_sigma^2 - 1)
+
+#b1 <- seq(0, 1, by = 0.01)    
+#by1 <- dbeta(b1, shape1 = 33.5, shape2 = 1083) 
+#plot(by1)
+
+
+nObs <- nrow(chile_bayes)
+nIter <- 1000
+nBurn <- 1000
+
+common_model <- "model {
+  theta ~ dbeta(theta_a, theta_b)
+  aut_sample ~ dbin(theta, nObs)
+  
+  spec ~ dnorm(spec_mu, 1/spec_sd) # dnorm requires prevalence not sd or var
+  sens ~ dnorm(sens_mu, 1/sens_sd)
+  
+  aut_pred ~ dbin(theta, nObs)
+  #aut_post <- aut_sample/nObs * sens + (1 - aut_sample/nObs) * spec
+}"
+
+common_data <- list(theta_a = theta_a, 
+                    theta_b = theta_b,
+                    nObs = nObs,
+                    aut_sample = sum(chile_bayes$autism),
+                    spec_mu = 0.996,
+                    spec_sd = (1.00-0.99) / (2*1.96),
+                    sens_mu = 0.62,
+                    sens_sd = (0.66-0.57) / (2*1.96))
+
+common_ini <- list(list(theta = 0.001, spec = 0.5, sens = 0.5),
+                   list(theta = 0.01, spec = 0.9, sens = 0.9)) 
+
+common_pars <- c("theta_a", "theta_b", "theta", "spec", "sens", "aut_sample", "aut_pred")
+
+# Run JAGS model and discard burn-in samples
+common_jag <- jags.model(textConnection(common_model),
+                         data = common_data,
+                         inits = common_ini,
+                         n.chains = 2,
+                         quiet = TRUE)
+update(common_jag, n.iter = nBurn)
+common_sam <- coda.samples(model = common_jag,
+                           variable.names = common_pars,
+                           n.iter = nIter)
+
+# Check for convergence in parameters of interest
+mcmc_trace(common_sam, common_pars)
+
+summary(as_draws(common_sam))
 
 
 # Set priors for prevalence, sensitivity and specificity of school-based autism assessment
 # Assume sensitivity and specificity are normally distributed
-aut_prev <- list(y_sample = count(filter(chile_slim, special_needs_code == "105")),
-                 n_sample = nrow(chile_slim), 
-                 spec_mu = 0.996, # from Barbaro et al
-                 spec_sd = (1.00-0.99) / (2*1.96), # from Barbaro et al, CI for spec is 0.99-1.00 (Joseph et al used 0.067 for survey and 0.004 for admin)
-                 sens_mu = 0.620, # from Barbaro et al, for SACS-R (excluding SACS-PR)
-                 sens_sd = (0.66-0.57) / (2*1.96), # from Barbaro et al, CI for sens is 0.57-0.66 (Joseph et al used 0.020 for survey and 0.020 for admin)
-                 p_mu = 0.5, # parameters of beta-proportion distribution, from Joseph et al
-                 p_kappa = 2 # ditto, kappa must be strictly positive
-                 #p_a = 0.5 * 2, # a = mu * kappa
-                 #p_b = (1-0.5) * 2 # b = (1-mu) * kappa
-)
+# aut_prev <- list(y_sample = count(filter(chile_slim, special_needs_code == "105")),
+#                  n_sample = nrow(chile_slim), 
+#                  spec_mu = 0.996, # from Barbaro et al
+#                  spec_sd = (1.00-0.99) / (2*1.96), # from Barbaro et al, CI for spec is 0.99-1.00 (Joseph et al used 0.067 for survey and 0.004 for admin)
+#                  sens_mu = 0.620, # from Barbaro et al, for SACS-R (excluding SACS-PR)
+#                  sens_sd = (0.66-0.57) / (2*1.96), # from Barbaro et al, CI for sens is 0.57-0.66 (Joseph et al used 0.020 for survey and 0.020 for admin)
+#                  p_mu = 0.5, # parameters of beta-proportion distribution, from Joseph et al
+#                  p_kappa = 2 # ditto, kappa must be strictly positive
+#                  #theta_a = 0.5 * 2, # a = mu * kappa
+#                  #theta_b = (1-0.5) * 2 # b = (1-mu) * kappa
+# )
 
-model <- stan_model("Autism-diagnosis-age-ML/single_test.stan")
+#model <- stan_model("Autism-diagnosis-age-ML/single_test.stan")
 
+
+# Is prevalence the same across geographic regions?
+aut_prev_by_school_region <- chile_bayes %>%
+  group_by(school_region_code, sex, autism) %>%
+  summarise(count = n()) %>%
+  pivot_wider(names_from = autism, values_from = count) %>%
+  mutate(prevalence = `1` / (`0` + `1`)) %>%
+  arrange(prevalence) %>%
+  left_join(distinct(select(chile, c("school_region_code", "school_region_name_abr"))), by = "school_region_code")
+aut_prev_by_school_region
+
+ggplot(data = aut_prev_by_school_region) +
+  geom_col(aes(x = school_region_name_abr, y = prevalence, group = sex, fill = as.factor(sex)), position = "dodge")
+
+# 6-8, 9-11, 12-14, 15-18
+# Stratify by age and sex
 
